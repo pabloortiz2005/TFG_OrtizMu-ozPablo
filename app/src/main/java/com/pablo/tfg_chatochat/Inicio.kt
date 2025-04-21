25541c16eef3927222546492c7d6c2f27b251a40
Pablo Ortiz Muñoz <pabloortiz22.04.2005@gmail.com>
2025-04-21 05:28:43 +0200

    Interfaz inicio y botones para el registro y el logeo

---
+++ app/src/main/java/com/pablo/tfg_chatochat/Inicio.kt
@@ -0,0 +1,44 @@
+package com.pablo.tfg_chatochat
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Inicio : AppCompatActivity() {
+
+    private lateinit var BotonIrRegistro: Button
+    private lateinit var BotonIrLogeo: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inicio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        BotonIrRegistro = findViewById(R.id.BotonIrRegistro)
+        BotonIrLogeo = findViewById(R.id.BotonIrLogeo)
+
+        BotonIrRegistro.setOnClickListener{
+            val intent = Intent(this@Inicio, RegistroActivity::class.java)
+            Toast.makeText(applicationContext, "Registros", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+
+        BotonIrLogeo.setOnClickListener{
+            val intent = Intent(this@Inicio, LoginActivity::class.java)
+            Toast.makeText(applicationContext, "Login", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+
+
+    }
+}
--- app/src/main/java/com/pablo/tfg_chatochat/RegistroActivity.kt
+++ app/src/main/java/com/pablo/tfg_chatochat/RegistroActivity.kt
@@ -1,38 +1,20 @@
 package com.pablo.tfg_chatochat
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Bienvenida : AppCompatActivity() {
+class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bienvenida)
+        setContentView(R.layout.activity_registro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        mostrarBienvenida()
-    }
-
-    fun mostrarBienvenida(){
-        object : CountDownTimer(3000, 1000){
-            override fun onTick(p0: Long) {
-
-            }
-
-            override fun onFinish() {
-                val intent = Intent(applicationContext, Inicio::class.java)
-                startActivity(intent)
-                finish()
-            }
-
-        }.start()
     }
 }
