5f292049349f24f56852d8dbadf78a08fd18d6db
Pablo Ortiz Muñoz <pabloortiz22.04.2005@gmail.com>
2025-04-23 12:44:16 +0200

    Registro usuarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
 }
--- app/src/main/java/com/pablo/tfg_chatochat/RegistroActivity.kt
+++ app/src/main/java/com/pablo/tfg_chatochat/RegistroActivity.kt
@@ -1,12 +1,29 @@
 package com.pablo.tfg_chatochat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var RegistroNombre: EditText
+    private lateinit var RegistroCorreo: EditText
+    private lateinit var RegistroContraseña: EditText
+    private lateinit var RegistroConfirmarContraseña: EditText
+    private lateinit var RegistroBotonRegistrar: Button
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var reference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +33,80 @@ class RegistroActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar!!.title = "Registros"
+        InicializarV()
+
+        RegistroBotonRegistrar.setOnClickListener {
+            ValidarDatos()
+        }
+    }
+
+    private fun InicializarV() {
+        RegistroNombre = findViewById(R.id.RegistroNombre)
+        RegistroCorreo = findViewById(R.id.RegistroCorreo)
+        RegistroContraseña = findViewById(R.id.RegistroContraseña)
+        RegistroConfirmarContraseña = findViewById(R.id.RegistroConfirmarContraseña)
+        RegistroBotonRegistrar = findViewById(R.id.RegistroBotonRegistrar)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun ValidarDatos() {
+        val nombre: String = RegistroNombre.text.toString()
+        val correo: String = RegistroCorreo.text.toString()
+        val contraseña: String = RegistroContraseña.text.toString()
+        val confirmarContraseña: String = RegistroConfirmarContraseña.text.toString()
+
+        if (nombre.isEmpty() || correo.isEmpty() || contraseña.isEmpty() || confirmarContraseña.isEmpty()) {
+            Toast.makeText(this, "Por favor, rellene todos los campos", Toast.LENGTH_SHORT).show()
+        } else if (contraseña != confirmarContraseña) {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+        } else {
+            RegistrarUsuario(nombre, correo, contraseña)
+        }
+    }
+
+    private fun RegistrarUsuario(nombre: String, correo: String, contraseña: String) {
+        auth.createUserWithEmailAndPassword(correo, contraseña)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    var uid: String = ""
+                    uid = auth.currentUser!!.uid
+                    reference =
+                        FirebaseDatabase.getInstance().reference.child("Usuarios").child(uid)
+
+                    val hashmap = HashMap<String, Any>()
+                    val h_nombre: String = RegistroNombre.text.toString()
+                    val h_correo: String = RegistroCorreo.text.toString()
+
+                    hashmap["uid"] = uid
+                    hashmap["nombre"] = h_nombre
+                    hashmap["correo"] = h_correo
+                    hashmap["imagen"] = ""
+                    hashmap["buscar"] = h_nombre.lowercase()
+
+                    reference.updateChildren(hashmap).addOnCompleteListener { task2 ->
+                        if (task2.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Usuario registrado correctamente",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(this@RegistroActivity, Inicio::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+
+                    }.addOnFailureListener { e ->
+                        Toast.makeText(
+                            this,
+                            "Error al registrar usuario: ${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }.addOnFailureListener { e ->
+                Toast.makeText(this, "Error al registrar usuario: ${e.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 }
