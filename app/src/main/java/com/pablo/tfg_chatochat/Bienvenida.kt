3fdf7942913153d52e104e1c1373c6339b421a7e
Pablo Ortiz Muñoz <pabloortiz22.04.2005@gmail.com>
2025-05-21 13:00:54 +0200

    Modo claro siempre

--- app/src/main/java/com/pablo/tfg_chatochat/Bienvenida.kt
+++ app/src/main/java/com/pablo/tfg_chatochat/Bienvenida.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Bienvenida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_bienvenida)
